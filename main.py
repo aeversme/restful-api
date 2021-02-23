4dee829d7538caf5c3b6fc36806cdffbb539a96b
Alex <aeversme@gmail.com>
2021-02-22 20:50:03 -0500

    create GET routes for random, all, and location search

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import random
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -23,23 +24,44 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
 
-## HTTP GET - Read Record
+
+# HTTP GET - Read Record
 @app.route('/random', methods=['GET'])
 def get_random_cafe():
-    pass
+    all_cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(all_cafes)
+    return jsonify(cafe=random_cafe.to_dict())
 
 
-## HTTP POST - Create Record
+@app.route('/all', methods=['GET'])
+def get_all_cafes():
+    all_cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
-## HTTP PUT/PATCH - Update Record
 
-## HTTP DELETE - Delete Record
+@app.route('/search')
+def search_for_cafe():
+    query_location = request.args.get('loc')
+    cafes_near_location = db.session.query(Cafe).filter_by(location=query_location).all()
+    if not cafes_near_location:
+        return jsonify(error={'Not Found': 'Sorry, there are no cafes near that location.'})
+    else:
+        return jsonify(cafe=[cafe.to_dict() for cafe in cafes_near_location])
+
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
